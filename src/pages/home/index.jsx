d92249cfcc8c0e49c478e109bdb363bfeb1f9193
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-15 10:16:49 -0300

    feat(home): add navigation home pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import bannerImage from '../../assets/banner.png';
 import { Header } from '../../components/Header';
@@ -8,36 +9,40 @@ import { Button } from '../../components/Button';
 // eslint-disable-next-line object-curly-newline
 import { Container, TextContent, Title, TitleHighlight } from './styles';
 
-// eslint-disable-next-line react/function-component-definition
-const Home = () => (
-  <>
-    <Header />
-    <Container>
-      <div>
-        <Title>
-          <TitleHighlight>
-            Implemente
-            <br />
-          </TitleHighlight>
-          o seu futuro global agora!
-        </Title>
-        <TextContent>
-          Domine as tecnologias utilizadas pelas empresas mais inovadora do
-          mundo e encare seu novo desafio profissional, evoluuindo em comunidade
-          com os melhores experts.
-        </TextContent>
-        <Button
-          title="Começar agora"
-          variant="secondary"
-          onClick={() => null}
-        />
-      </div>
-      <div>
-        <img src={bannerImage} alt="Image principal" />
-      </div>
-    </Container>
-  </>
-);
+export default function Home() {
+  const navigate = useNavigate();
 
-// eslint-disable-next-line import/prefer-default-export
-export { Home };
+  const handleClickSignIn = () => {
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div>
+          <Title>
+            <TitleHighlight>
+              Implemente
+              <br />
+            </TitleHighlight>
+            o seu futuro global agora!
+          </Title>
+          <TextContent>
+            Domine as tecnologias utilizadas pelas empresas mais inovadora do
+            mundo e encare seu novo desafio profissional, evoluuindo em
+            comunidade com os melhores experts.
+          </TextContent>
+          <Button
+            title="Começar agora"
+            variant="secondary"
+            onClick={handleClickSignIn}
+          />
+        </div>
+        <div>
+          <img src={bannerImage} alt="Image principal" />
+        </div>
+      </Container>
+    </>
+  );
+}
