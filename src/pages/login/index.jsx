54a6223ccf93ebb30488768c92c831be1045edc4
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-15 10:16:03 -0300

    feat(login): add navigation login pages

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import { MdEmail, MdLock } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
@@ -19,35 +20,48 @@ import {
   Wrapper,
 } from './styles';
 
-// eslint-disable-next-line react/function-component-definition
-const Login = () => (
-  <>
-    <Header />
-    <Container>
-      <Column>
-        <Title>
-          A plataforma para você aprender com experts, dominar as principais
-          tecnologias e entrar mais rápido nas empresas mais desejadas.
-        </Title>
-      </Column>
-      <Column>
-        <Wrapper>
-          <TitleLogin>Faça seu cadastro</TitleLogin>
-          <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-          <form>
-            <Input placeholder="Email" leftIcon={<MdEmail />} />
-            <Input placeholder="Senha" type="password" leftIcon={<MdLock />} />
-            <Button title="Entrar" variant="secondary" />
-          </form>
-          <Row>
-            <EsqueciText>Esqueci minha senha</EsqueciText>
-            <CriarText>Criar Conta</CriarText>
-          </Row>
-        </Wrapper>
-      </Column>
-    </Container>
-  </>
-);
+export default function Login() {
+  const navigate = useNavigate();
 
-// eslint-disable-next-line import/prefer-default-export
-export { Login };
+  const handleClickSignIn = () => {
+    navigate('/feed');
+  };
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu cadastro</TitleLogin>
+            <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+            <form>
+              <Input placeholder="Email" leftIcon={<MdEmail />} />
+              <Input
+                placeholder="Senha"
+                type="password"
+                leftIcon={<MdLock />}
+              />
+              <Button
+                title="Entrar"
+                variant="secondary"
+                onClick={handleClickSignIn}
+                type="button"
+              />
+            </form>
+            <Row>
+              <EsqueciText>Esqueci minha senha</EsqueciText>
+              <CriarText>Criar Conta</CriarText>
+            </Row>
+          </Wrapper>
+        </Column>
+      </Container>
+    </>
+  );
+}
