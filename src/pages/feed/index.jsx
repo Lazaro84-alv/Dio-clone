07d5af77649f913ff39fe03cd0f32f1ad662205a
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-15 09:31:23 -0300

    feat(feed): implements in feed pages

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -3,16 +3,55 @@ import React from 'react';
 
 import { Header } from '../../components/Header';
 import { Card } from '../../components/Card';
+import { UserInfo } from '../../components/UserInfo';
 
 // eslint-disable-next-line object-curly-newline
-import { Container } from './styles';
+import { Container, Column, Title, TitleHighlight } from './styles';
 
 // eslint-disable-next-line react/function-component-definition
 const Feed = () => (
   <>
     <Header />
     <Container>
-      <Card />
+      <Column flex={3}>
+        <Title>Feed</Title>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Column>
+      <Column flex={1}>
+        <TitleHighlight># RANKING 5 TOP DA SEMANA</TitleHighlight>
+        <UserInfo
+          percentual={45}
+          nome="Lázaro Alves"
+          image="https://avatars.githubusercontent.com/u/75143575?v=4"
+        />
+        <UserInfo
+          percentual={87}
+          nome="Lázaro Alves"
+          image="https://avatars.githubusercontent.com/u/75143575?v=4"
+        />
+        <UserInfo
+          percentual={27}
+          nome="Lázaro Alves"
+          image="https://avatars.githubusercontent.com/u/75143575?v=4"
+        />
+        <UserInfo
+          percentual={56}
+          nome="Lázaro Alves"
+          image="https://avatars.githubusercontent.com/u/75143575?v=4"
+        />
+        <UserInfo
+          percentual={13}
+          nome="Lázaro Alves"
+          image="https://avatars.githubusercontent.com/u/75143575?v=4"
+        />
+      </Column>
     </Container>
   </>
 );
--- src/pages/feed/styles.js
+++ src/pages/feed/styles.js
@@ -2,38 +2,36 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Container = styled.main`
-  width: 100%;
-  max-width: 80%;
-  margin: 0 auto;
-  margin-top: 120px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+ idth: 100%;
+    max-width: 80%;
+    margin: 0 auto;
+    margin-top: 120px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `;
 
-export const Title = styled.h2`
+export const Title = styled.h3`
   font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 32px;
-  width: 320px;
-  margin-bottom: 20px;
-  line-height: 44px;
-  color: #FFFFFF;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 25px;
+    color: #FFFFFF;
+    margin-bottom: 24px;
 `;
 
-export const TitleHighlight = styled.span`
-  color: #e4105d;
-`;
-
-export const TextContent = styled.p`
+export const TitleHighlight = styled.h3`
   font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  width: 420px;
-  margin-bottom: 20px;
-  line-height: 22px;
-  color: #FFFFFF;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 25px;
+    color: #FFFFFF70;
+    margin-bottom: 24px;
+`;
+
+export const Column = styled.div`
+    flex:${({ flex }) => flex};
+    padding-right: 24px;
 `;
