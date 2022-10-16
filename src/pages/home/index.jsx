13faf82d6008dd0af609427bb3514c71087b9d9c
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-15 23:36:33 -0300

    feat(login): implements validation with react hook forms in login pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 import bannerImage from '../../assets/banner.png';
 import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
+import Button from '../../components/Button';
 
 // eslint-disable-next-line object-curly-newline
 import { Container, TextContent, Title, TitleHighlight } from './styles';
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,10 +2,13 @@
 import React from 'react';
 import { MdEmail, MdLock } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { api } from '../../services/api';
 
 // eslint-disable-next-line object-curly-newline
 import {
@@ -23,9 +26,32 @@ import {
 export default function Login() {
   const navigate = useNavigate();
 
-  const handleClickSignIn = () => {
-    navigate('/feed');
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    reValidateMode: 'onChange',
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (formData) => {
+    try {
+      const { data } = await api.get(
+        `/users?email=${formData.email}&senha=${formData.senha}`,
+      );
+
+      if (data.length && data[0].id) {
+        navigate('/feed');
+        return;
+      }
+      alert('Usuário ou senha inválido');
+    } catch (e) {
+      // TODO: HOUVE UM ERRO
+    }
   };
+
+  console.log('errors', errors);
 
   return (
     <>
@@ -34,26 +60,31 @@ export default function Login() {
         <Column>
           <Title>
             A plataforma para você aprender com experts, dominar as principais
-            tecnologias e entrar mais rápido nas empresas mais desejadas.
+            tecnologias tecnologias e entrar mais rápido nas empresas mais
+            desejadas.
           </Title>
         </Column>
         <Column>
           <Wrapper>
             <TitleLogin>Faça seu cadastro</TitleLogin>
             <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-            <form>
-              <Input placeholder="Email" leftIcon={<MdEmail />} />
+            <form onSubmit={handleSubmit(onSubmit)}>
               <Input
-                placeholder="Senha"
+                placeholder="E-mail"
+                leftIcon={<MdEmail />}
+                name="email"
+                control={control}
+              />
+              {errors.email && <span>E-mail é obrigatório</span>}
+              <Input
                 type="password"
+                placeholder="Senha"
                 leftIcon={<MdLock />}
+                name="senha"
+                control={control}
               />
-              <Button
-                title="Entrar"
-                variant="secondary"
-                onClick={handleClickSignIn}
-                type="button"
-              />
+              {errors.senha && <span>Senha é obrigatório</span>}
+              <Button title="Entrar" variant="secondary" type="submit" />
             </form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
