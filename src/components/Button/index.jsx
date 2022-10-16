56e666d9e49c158400e1693a68a4f5a3cd0fade2
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-15 23:32:53 -0300

    feat(button): implements button components

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 import { ButtonContainer } from './styles';
 
-const Button = ({title,variant = "primary", onClick}) => {
+// eslint-disable-next-line react/function-component-definition
+// eslint-disable-next-line react/prop-types
+export default function Button({ title, variant = 'primary', onClick }) {
   return (
-    <ButtonContainer variant={variant} onClick={onClick}>{title}</ButtonContainer>
-  )
+    <ButtonContainer variant={variant} onClick={onClick}>
+      {title}
+    </ButtonContainer>
+  );
 }
-
-export { Button }
